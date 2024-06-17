935e507d97b142d1be6bec144adb11401d3ccb2a
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-06-17 20:37:17 -0300

    add: findById en ticket

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -14,6 +14,26 @@ ticketController.getAll = async (req, res) => {
   res.json({ data: tickets });
 };
 
+ticketController.getById = async (req, res) => {
+  const id = req.params.id;
+  if (!id && !isValidObjectId(id)) {
+    return res.status(400).json({
+      status: '400',
+      message: 'El ID no es válido',
+    });
+  }
+
+  try {
+    const ticket = await TicketModel.findById(id);
+    res.json({ data: ticket });
+  } catch (err) {
+    res.status(404).json({
+      status: '404',
+      message: `No se ha encontrado el ticket con el id '${id}'`,
+    });
+  }
+};
+
 ticketController.create = async (req, res) => {
   const ticket = new TicketModel(req.body);
 
--- src/routes/ticket.route.js
+++ src/routes/ticket.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const ticketController = require('../controllers/ticket.controller');
 
 router.get('/', ticketController.getAll);
+router.get('/:id', ticketController.getById);
 router.post('/', ticketController.create);
 router.patch('/', ticketController.update);
 router.delete('/:id', ticketController.deleteById);
